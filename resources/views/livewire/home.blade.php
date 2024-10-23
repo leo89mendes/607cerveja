204824a003dcf68ee3aa3dfcd78565482ccc5dd4
Leonardo Mendes <leo.89mendes@gmail.com>
2024-10-23 18:30:31 -0300

    atualizando

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,15 @@
+<div class="container bg-cover bg-center h-screen w-full" style="background-image: url('storage/assets/img/bannerbeer.jpg')">
+    <div class="flex justify-center flex-col items-center h-full">
+        <div class=" w-full flex justify-center">
+            <img src="{{ asset('storage/assets/img/logo.png')}}" alt="">
+        </div>
+        <h2 class="text-white bg-black font-bold py-4 mt-4 text-3xl text-center">Em Breve estaremos inaugurando nosso novo site, aguardem.</h2>
+        <h3 class="text-white bg-black font-bold py-4 mt-4 text-3xl text-center">Visite nosso instagram: <a class=" hover:text-[#4caf50]" href="https://instagram.com/607cervejaartesanal" target="_blank">@607cervejaartesanal</a></h3>
+        <h3 class="text-white font-bold py-4 mt-4 text-3xl text-center">
+            <a target="_blank" href="https://wa.me/11976611435">    
+                <img alt="Fale conosco" width="65" src="{{ asset('storage/assets/img/whatsapp.png') }}"/>
+            </a>
+        </h3>
+        
+    </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import preset from './vendor/filament/support/tailwind.config.preset'
 
+export default {
+  presets: [preset],
+  content: [
+      './app/Filament/**/*.php',
+      './resources/views/**/*.blade.php',
+      './vendor/filament/**/*.blade.php',
+  ],
+  theme: {
+    extend: {
+      backgroundImage:{
+          'bgbanner': "url('/storage/assets/img/bannerbeer.jpg')",
+        }
+    }
+  }
+}
