48bea76db91388ed526008ad3ab66bc9111dcabb
Leonardo Mendes <leo.89mendes@gmail.com>
2024-10-23 22:31:24 -0300

    subindo ajustes

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,7 +7,16 @@
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>{{ config('app.name') }}</title>
+        <title>607 Cerveja Artesanal</title>
+        <meta name="keywords" content="Cerveja, Cerveja Artesanal, 607 Cerveja">
+        <meta name="description" content="A 607 foi criada da vontade de tomar boas cervejas e oferecer produtos de qualidade para amigos, que viraram clientes. Ainda com a produção totalmente artesanal continuamos com a ideia de produzir cerveja com amor e os melhores insumos para levar até vocês a melhor cerveja. Bom apetite!">
+        <meta name="author" content="Dreams Into Codes">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="607 Cerveja Artesanal">
+        <meta property="og:description" content="A 607 foi criada da vontade de tomar boas cervejas e oferecer produtos de qualidade para amigos, que viraram clientes. Ainda com a produção totalmente artesanal continuamos com a ideia de produzir cerveja com amor e os melhores insumos para levar até vocês a melhor cerveja. Bom apetite!">
+        <meta property="og:url" content="{{ url('/')}}">
+        <meta property="og:image" content="{{ asset('storage/assets/theme.png') }}">
+        <!-- <link rel="icon" type="image/x-icon" href="{{ asset('storage/assets/favicon.ico') }}"> -->
 
         <style>
             [x-cloak] {
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -3,9 +3,9 @@
         <div class=" w-full flex justify-center">
             <img src="{{ asset('storage/assets/img/logo.png')}}" alt="">
         </div>
-        <h2 class="text-white bg-black font-bold py-4 mt-4 text-3xl text-center">Em Breve estaremos inaugurando nosso novo site, aguardem.</h2>
-        <h3 class="text-white bg-black font-bold py-4 mt-4 text-3xl text-center">Visite nosso instagram: <a class=" hover:text-[#4caf50]" href="https://instagram.com/607cervejaartesanal" target="_blank">@607cervejaartesanal</a></h3>
-        <h3 class="text-white font-bold py-4 mt-4 text-3xl text-center">
+        <h2 class="text-white bg-black font-bold p-4 mt-4 text-3xl text-center">Em Breve estaremos inaugurando nosso novo site, aguardem.</h2>
+        <h3 class="text-white bg-black font-bold p-4 mt-4 text-3xl text-center">Visite nosso instagram: <a class=" hover:text-[#4caf50]" href="https://instagram.com/607cervejaartesanal" target="_blank">@607cervejaartesanal</a></h3>
+        <h3 class="text-white font-bold p-4 mt-4 text-3xl text-center">
             <a target="_blank" href="https://wa.me/11976611435">    
                 <img alt="Fale conosco" width="65" src="{{ asset('storage/assets/img/whatsapp.png') }}"/>
             </a>
